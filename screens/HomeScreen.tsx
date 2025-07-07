1d0aa3c236e548a56bcf358a64be1a23b1c999da
Romil Movaliya <105592033+RomilMovaliya@users.noreply.github.com>
2025-07-07 09:52:18 +0530

    Revert "Feat rm homescreen1"

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,62 +1,14 @@
-import { FlatList, StyleSheet, Text, View, Dimensions } from 'react-native'
-import React, { useRef, useState } from 'react'
-import { videos } from '../assets/data'
-import VideoWrapper from '../components/VideoWrapper'
-
-const { height: screenHeight } = Dimensions.get('window')
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const HomeScreen = () => {
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const flatListRef = useRef(null)
-
-    const onViewableItemsChanged = ({ viewableItems }: { viewableItems: Array<{ index: number | null }> }) => {
-        if (viewableItems.length > 0 && viewableItems[0].index !== null) {
-            setCurrentIndex(viewableItems[0].index)
-        }
-    }
-
-    const viewabilityConfig = {
-        itemVisiblePercentThreshold: 50
-    }
-
     return (
-        <View style={styles.container}>
-            <FlatList
-                ref={flatListRef}
-                keyExtractor={(item) => item.id.toString()}
-                data={videos}
-                renderItem={({ item, index }) => {
-                    return (
-                        <VideoWrapper
-                            data={item}
-                            isActive={index === currentIndex}
-                            index={index}
-                        />
-                    )
-                }}
-                pagingEnabled
-                horizontal={false}
-                showsVerticalScrollIndicator={false}
-                snapToInterval={screenHeight}
-                snapToAlignment="start"
-                decelerationRate="fast"
-                onViewableItemsChanged={onViewableItemsChanged}
-                viewabilityConfig={viewabilityConfig}
-                getItemLayout={(data, index) => ({
-                    length: screenHeight,
-                    offset: screenHeight * index,
-                    index,
-                })}
-            />
+        <View>
+            <Text>HomeScreen</Text>
         </View>
     )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#000',
-    }
-})
+const styles = StyleSheet.create({})
